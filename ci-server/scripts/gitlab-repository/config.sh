8de96a20a6947a9cff5a741244fd5c0b8835daf6
Second User <seconduser@uni.lu>
2023-10-18 12:02:00 +0200

    Update IP address of CI server

--- ci-server/scripts/gitlab-repository/config.sh
+++ ci-server/scripts/gitlab-repository/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Send POST request to API using token generated for the user Flavio to create the repository
-curl --header "PRIVATE-TOKEN: abcdefghijklmnopqrstuvwxyz" -X POST "http://192.168.33.96/gitlab/api/v4/projects?name=MavenHelloWorldProject"
+curl --header "PRIVATE-TOKEN: abcdefghijklmnopqrstuvwxyz" -X POST "http://192.168.33.94/gitlab/api/v4/projects?name=MavenHelloWorldProject"
 
 # Install GitLab Runner
 curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash
@@ -13,7 +13,7 @@ registration_token=$(sudo gitlab-rails runner -e production " puts Gitlab::Curre
 # Register runner (without tags)
 sudo gitlab-runner register \
     --non-interactive \
-    --url "http://192.168.33.96/gitlab" \
+    --url "http://192.168.33.94/gitlab" \
     --registration-token "$registration_token" \
     --description "docker" \
     --tag-list "integration" \
