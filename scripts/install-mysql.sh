ff98e5a2ef64cedd21c8b03cadabdf74c0b838d7
Flavio <flavio8699@gmail.com>
2023-10-18 15:17:32 +0200

    Update install mysql script

--- scripts/install-mysql.sh
+++ scripts/install-mysql.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND="noninteractive"
 echo "Install MySql server"
 sudo debconf-set-selections <<< 'mysql-community-server mysql-community-server/re-root-pass password 12345678'
 sudo debconf-set-selections <<< 'mysql-community-server mysql-community-server/root-pass password 12345678'
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password 12345678"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 12345678"
+
 sudo -E apt-get -y install mysql-server --allow-unauthenticated
