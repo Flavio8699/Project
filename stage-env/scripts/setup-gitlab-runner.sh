f6e4b8641acf94e3be74be849b7fb58ef9ffc06c
Flavio Matias <flavio8699@gmail.com>
2023-10-25 20:26:31 +0200

    Add stage env provision scripts

---
+++ stage-env/scripts/setup-gitlab-runner.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install GitLab Runner
+curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash
+sudo apt-get install gitlab-runner
+
+# Get runner token
+registration_token=$(cat /shared-data/gitlab-runner-token.txt)
+
+# Register runner (without tags)
+sudo gitlab-runner register \
+    --non-interactive \
+    --url "http://192.168.33.94/gitlab" \
+    --registration-token "$registration_token" \
+    --description "shell" \
+    --tag-list "stage-vm-shell" \
+    --executor "shell"
+
+# Restart runner
+sudo gitlab-runner restart
+
+# Add gitlab-runner user to vagrant's group
+sudo usermod -aG vagrant gitlab-runner
