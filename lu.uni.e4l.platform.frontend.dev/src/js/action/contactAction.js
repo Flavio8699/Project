03359dc2600f89b73a4d95bc360bd15b538d77bb
Tim Kieffer <55922216+TimKie@users.noreply.github.com>
2023-10-28 11:47:26 +0200

    Delete lu.uni.e4l.platform.frontend.dev directory

--- lu.uni.e4l.platform.frontend.dev/src/js/action/contactAction.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios/index";
-
-export function sendMessage(message) {
-    delete axios.defaults.headers.common["Authorization"];
-    return {
-        type: "SEND_MESSAGE",
-        payload: axios.post("/contact", message)
-    }
-}
