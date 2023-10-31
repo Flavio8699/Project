423dea3cdd54946094f4b2bb8ea8cb640b62ccab
Flavio Matias <flavio8699@gmail.com>
2023-10-31 12:47:31 +0100

    Create setup nginx script

---
+++ stage-env/scripts/setup-nginx.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Create nginx configuration file
+conf=$(cat <<'EOF'
+server {
+    listen       80;
+
+    location /staticnews {
+        root   /home/vagrant/frontend/news;
+        index  index.html index.htm;
+
+        # disable cache
+        add_header Last-Modified $date_gmt;
+        add_header Cache-Control 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0';
+        if_modified_since off;
+        expires off;
+        etag off;
+
+        try_files $uri $uri/ =404;
+    }
+
+    location / {
+        root   /home/vagrant/frontend/html;
+        index  index.html index.htm;
+
+	    try_files $uri $uri/ /index.html;
+    }
+}
+EOF
+)
+echo "$conf" | sudo tee /etc/nginx/conf.d/default.conf > /dev/null
+
+# Restart nginx
+sudo service nginx restart
