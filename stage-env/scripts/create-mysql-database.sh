73f84902af85bc0d5eace8d24c5f321672297315
Flavio Matias <flavio8699@gmail.com>
2023-10-31 13:14:29 +0100

    Check if database already exists

--- stage-env/scripts/create-mysql-database.sh
+++ stage-env/scripts/create-mysql-database.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-# Create MySQL databse
-mysql -u root --password=12345678 -e "create database e4l";
+# MySQL database details
+db_user="root"
+db_password="12345678"
+db_name="e4l"
+
+# Check if the database exists
+if mysql -u "$db_user" --password="$db_password" -e "USE $db_name" >/dev/null 2>&1; then
+    # Delete if it exists
+    mysql -u "$db_user" --password="$db_password" -e "DROP DATABASE $db_name"
+fi
+
+# Create the database
+mysql -u "$db_user" --password="$db_password" -e "CREATE DATABASE $db_name"
