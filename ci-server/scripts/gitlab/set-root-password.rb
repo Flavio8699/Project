35f474820c8a9055a561e9d5530fa1c94ee4d8df
TimKie <kieffertim47@gmail.com>
2023-10-31 15:51:18 +0100

    Check if root pwd was already set before creation

--- ci-server/scripts/gitlab/set-root-password.rb
+++ ci-server/scripts/gitlab/set-root-password.rb
@@ -1,6 +1,12 @@
 #!/usr/bin/env ruby
 
 user = User.where(id: 1).first # get first user in database (which is the admin)
-user.password = '12345678' # set password
-user.password_confirmation = '12345678' # confirm password
-user.save! # save user
+# Check if the user's password is already set
+if user && user.encrypted_password.blank?
+    user.password = '12345678' # set the password
+    user.password_confirmation = '12345678' # confirm the password
+    user.save! # save the user
+    puts "Root password has been set."
+  else
+    puts "Root password already exists. Skipping script."
+  end
